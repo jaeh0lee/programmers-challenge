0c8dc0db5ff2c24897b767582f217450b19494da
STRONGSHARK <woghfghf@naver.com>
2023-01-26 03:48:12 +0900

    7일차

--- lv.0/짝수 홀수 개수.js
+++ lv.0/짝수 홀수 개수.js
@@ -1,14 +1,12 @@
 function solution(num_list) {
-  let cnt = 0;
-  let odd = 0;
   let even = 0;
-
+  let add = 0;
   for (let i = 0; i < num_list.length; i++) {
     if (num_list[i] % 2 === 0) {
       even++;
     } else {
-      odd++;
+      add++;
     }
   }
-  return [even, odd];
+  return [even, add];
 }
